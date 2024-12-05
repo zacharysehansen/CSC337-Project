3414084192f09afa230af594a2e984da1709c409
zacharysehansen <115272273+zacharysehansen@users.noreply.github.com>
2024-12-04 23:07:46 -0700

    server and tank display
    I made some changes to the css to make the tank display properly. I also changed the server code to take JSON files until the database is set up

--- public_html/scripts/back_end/index.js
+++
@@ -1,5 +0,0 @@
-const User = require('./User');
-const Fish = require('./Fish');
-const Leaderboard = require('./Leaderboard');
-
-module.exports = { User, Fish, Leaderboard };
--- public_html/scripts/front_end/script.js
+++ public_html/scripts/front_end/script.js
@@ -1,50 +1,60 @@
-// make a fish object
-const fish = {
-  x: 100, // start x
-  y: 100, // start y
-  width: 64, // Width of the fish element
-  height: 64, // Height of the fish element
-  speed: 2, // speed can be realy fast 4 fun
-  angle: Math.random() * Math.PI * 2, // random initial swim angle
-};
+// make fish objects for each fish in the container
+const fishes = [];
 
-// get DOM element for fish
-const startFish = document.getElementById('startFish');
+// get DOM elements for all fish
+const fishContainer = document.getElementById('fishContainer');
+const fishElements = fishContainer.getElementsByTagName('img');
+
+// Convert existing fish elements into fish objects with their properties
+for (const fishElement of fishElements) {
+    fishes.push({
+        x: 100, // start x
+        y: 100, // start y
+        width: 64, // Width of the fish element
+        height: 64, // Height of the fish element
+        speed: 2, // speed can be realy fast 4 fun
+        angle: Math.random() * Math.PI * 2, // random initial swim angle
+        element: fishElement // store reference to the DOM element
+    });
+}
 
 // Define the aquarium boundaries
 const aquarium = document.getElementById('aquarium');
-const aquariumWidth = aquarium.offsetWidth;
-const aquariumHeight = aquarium.offsetHeight;
+const aquariumWidth = aquarium.clientWidth;
+const aquariumHeight = aquarium.clientHeight;
 
 // updates the fish position and angle
-function updateFish() {
-  // move the fish
-  fish.x += fish.speed * Math.cos(fish.angle);
-  fish.y += fish.speed * Math.sin(fish.angle);
+function updateFish(fish) {
+    // move the fish
+    fish.x += fish.speed * Math.cos(fish.angle);
+    fish.y += fish.speed * Math.sin(fish.angle);
 
-  // Keep the fish within the aquarium boundaries
-  if (fish.x < 0 || fish.x > aquariumWidth - fish.width) {
-    fish.angle = Math.PI - fish.angle; // reverse horizontal direction
-  }
-  if (fish.y < 0 || fish.y > aquariumHeight - fish.height) {
-    fish.angle = -fish.angle; // reverse vertical direction
-  }
+    // Keep the fish within the aquarium boundaries
+    if (fish.x < 0 || fish.x > aquariumWidth - fish.width) {
+        fish.angle = Math.PI - fish.angle; // reverse horizontal direction
+    }
+    if (fish.y < 0 || fish.y > aquariumHeight - fish.height) {
+        fish.angle = -fish.angle; // reverse vertical direction
+    }
 
-  // apply the updated position to the DOM element
-  startFish.style.left = `${fish.x}px`;
-  startFish.style.top = `${fish.y}px`;
+    // apply the updated position to the DOM element
+    fish.element.style.left = `${fish.x}px`;
+    fish.element.style.top = `${fish.y}px`;
 }
 
 // initialize and animate the fish
 function animateFish() {
-  updateFish();
-  requestAnimationFrame(animateFish);
+    // Update each fish in the aquarium
+    fishes.forEach(updateFish);
+    requestAnimationFrame(animateFish);
 }
 
-// set the initial position of the fish
-startFish.style.position = 'absolute';
-startFish.style.left = `${fish.x}px`;
-startFish.style.top = `${fish.y}px`;
+// set the initial position of all fish
+fishes.forEach(fish => {
+    fish.element.style.position = 'absolute';
+    fish.element.style.left = `${fish.x}px`;
+    fish.element.style.top = `${fish.y}px`;
+});
 
 // start the animation
 animateFish();
