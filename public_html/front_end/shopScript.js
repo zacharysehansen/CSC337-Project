2168bcb930e21928ab113b26ca170ce4c3b1b7e5
sameeka03 <148290755+sameeka03@users.noreply.github.com>
2024-12-13 18:40:53 -0700

    Update shopScript.js

--- public_html/front_end/shopScript.js
+++ public_html/front_end/shopScript.js
@@ -1,24 +1,14 @@
-// this is the javascript file for the index.html page. it gets all the stuff for the shop
-// done, including purchasing of fish, and the inital loading. this is simple a pop-up screen
-
-/* 
-this is a created pop up uing a 'modal box'
-a modal is a dialog box/popup window that is displayed on top of the current page
-
-this code was adjusted by me (jordan) but is directly edited from w3 schools "How TO - CSS/JS Modal"
-*/
-
 /*
-This section below is for loading the modal for the shop pop up
+Team: Sameeka Maroli, Zachary Hanse, Jordan Demler
+CSC 337: Pocket Pond
+This JavaScript file manages the functionality for the shop in the index.html page, using a modal box to create a pop-up shop interface. 
+It handles the initial loading of the shop, purchasing fish, and integrates a modal dialog adapted from W3Schools' tutorial on modals.
 */
 
-// get the modal from the index.html page
 var modal = document.getElementById("shopModal");
 
-// get the button that opens the modal from the index.html page
 var button = document.getElementById("shopButton");
 
-// get the button element that closes the modal from shop.html
 var close = document.getElementById("closeShopButton");
 
 // when the user clicks on the button, open the modal
@@ -26,12 +16,10 @@ button.onclick = function() {
   modal.style.display = "block";
 }
 
-// when the user clicks on the clsoe button, close the modal
 close.onclick = function() {
   modal.style.display = "none";
 }
 
-// when the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
